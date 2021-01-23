70917de9d738828ed5c945cd25904ed8b176252b
marcus <marcusnathanhill@outlook.com>
2021-01-23 13:30:55 +0000

    add: hose addon weapon (finally)

--- resource/fxmanifest.lua
+++ resource/fxmanifest.lua
@@ -13,4 +13,25 @@ server_scripts {
     'Server.net.dll',
 }
 
+files {
+    'hose/contentunlocks.meta',
+	'hose/loadouts.meta',
+	'hose/pedpersonality.meta',
+	'hose/shop_weapon.meta',
+	'hose/weaponanimations.meta',
+	'hose/weaponarchetypes.meta',
+	'hose/weapons.meta',
+}
+
+data_file 'WEAPONINFO_FILE' 'hose/weapons.meta'
+data_file 'WEAPON_METADATA_FILE' 'hose/weaponarchetypes.meta'
+data_file 'WEAPON_SHOP_INFO' 'hose/shop_weapon.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'hose/weaponanimations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'hose/contentunlocks.meta'
+data_file 'LOADOUTS_FILE' 'hose/loadouts.meta'
+data_file 'PED_PERSONALITY_FILE' 'hose/pedpersonality.meta'
+
 -- Join the London Studios Discord here: https://discord.gg/F2zmUTD
+
+-- Purchase the new Supply Line to integrate in with HoseLS here: https://store.londonstudios.net/category/1704957
+-- The Supply Line resource removes the infinite water supply, requiring you to setup supply lines with fire vehicles
